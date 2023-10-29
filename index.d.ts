bb32cbc5c61abf479e7d21a32744db4ffc7d0935
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-10-29 15:57:13 +0000

    init

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import type { SvelteHTMLElements, EventHandler } from 'svelte/elements';
+
+type Nullish<T> = T | null | undefined;
+
+/**
+ * Provides the event handler for a given element.
+ *
+ * @example
+ *
+ * type MyEventHandler = GetEventHandlers<"input">; // "on:change" | "on:copy" ...
+ *
+ */
+type GetEventHandlers<T extends keyof SvelteHTMLElements> = Extract<
+	keyof SvelteHTMLElements[T],
+	`on:${string}`
+>;
+
+/**
+ * Provides the event type for a given element and handler.
+ *
+ * @example
+ *
+ * type MyEvent = EventFor<"input", "on:change">; // Event & { currentTarget: EventTarget & HTMLInputElement }
+ *
+ */
+export type SvelteHTMLElementEvent<
+	TElement extends keyof SvelteHTMLElements,
+	THandler extends GetEventHandlers<TElement>
+> = SvelteHTMLElements[TElement][THandler] extends Nullish<EventHandler<infer TEvent, infer _>>
+	? TEvent
+	: never;
